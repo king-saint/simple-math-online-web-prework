30b5a0e11f8fa5c1c8b7c142ea0cb3cf2340c03f
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-21 01:07:55 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,33 @@
 def addition(num1, num2)
+  sum = num1 + num2 
+  sum
 end
 
 def subtraction(num1, num2)
+    fin = num1 - num2
 end
 
 def division(num1, num2)
+  arp = num1 / num2
+  arp
 end
 
 def multiplication(num1, num2)
+  garp = num1 * num2
+  garp
 end
 
 def modulo(num1, num2)
+  remain = num1 % num2
+  remain
 end
 
 def square_root(num)
+  eejit = Math.sqrt(num)
+  eejit
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  trunk  = num1 + ((num2 * num3) / num4)
+  trunk
 end
